4f8a88253b2f7c2250aba295508d1e14f004c29c
Alexandru Calugari <afishr00@gmail.com>
2020-11-16 16:54:13 +0200

    Basic app

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-const app = require('express');
+const express = require('express');
+const cookieParser = require('cookie-parser')
+const app = express();
+
+const server = app.listen(3000, () => {
+	console.log('Listening on port 3000');
+})
+
+app.set('view engine', 'pug')
+
+app.use(cookieParser());
+
+app.use((req, res, next) => {
+  console.log('%s %s', req.method, req.url);
+  next();
+});
+
+app.get('/', (req, res) => {
+
+  res.send('Hello');
+});
